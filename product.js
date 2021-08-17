20d20f1a2b48fb55025cbf49b9aec48b9c606855
yenia-m <88461546+yenia-m@users.noreply.github.com>
2021-08-16 21:59:39 -0300

    primer-sub

---
+++ product.js
@@ -0,0 +1,25 @@
+const productos_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+
+function cargarDatos(productos_URL){
+    document.getElementById("data").innerHTML="";
+    fetch(productos_URL)
+        .then(respuesta => respuesta.json())
+        .then(product => {
+            let row ="";
+            for(let i=0; i <product.length; i++){
+
+                row = 
+                `
+                <td>` + product[i].name + `</td>
+                <td>` + product[i].description + `</td>
+                <td>` + product[i].cost + `</td>
+                <td>` + product[i].currency + `</td>
+                
+                `;
+                  document.getElementById("data").innerHTML += row;
+    
+            }
+        })
+
+    
+}
